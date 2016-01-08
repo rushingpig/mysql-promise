d98eaaac0790affa5d09dcf2deae71c43f2b5cae
pigo <zhenglin.zhu@xfxb.net>
2016-01-08 11:09:18 +0800

    istanbu file

---
+++ test.js
@@ -0,0 +1,9 @@
+/**
+ * @des    :
+ * @author : pigo.can
+ * @date   : 16/1/8 上午11:07
+ * @email  : zhenglin.zhu@xfxb.net
+ * @version: v0.0.1
+ */
+"use strict";
+console.log('my name is promise...');
